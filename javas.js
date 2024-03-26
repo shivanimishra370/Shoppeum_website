4958a9298efbbee4ae27aeb8ca57689ee5c17e55
shivanimishra370 <105150905+shivanimishra370@users.noreply.github.com>
2024-03-26 11:53:53 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,13 @@
+// let cars=['maruti','honda', "hero", "xuv", "bmw"];
+// // cars.splice(0,1);
+// console.log(cars);
+// cars.splice(2,0, "a","s");
+// console.log(cars);
+let month=['jan','july','march','august'];
+month.splice(0,2,'july','june');
+console.log(month);
+//getElementbyCLassName"
+let smallImages=document.getElementsByClassName("oldImages");
+for(let i=0;i<smallImages.length;i++){
+    console.der(smallimages[i]);
+}
